1787fd6cde3512d848b359bdd0f3dcc9106f4ee5
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2022-01-21 14:12:30 -0500

    Implement corresponding method for matching keys for KIT data
    This commit implements a method to find matching keys for KIT data and
    returns a dictionary

--- src/verifiers/verifier_utils.py
+++ src/verifiers/verifier_utils.py
@@ -20,3 +20,23 @@ def is_between(comp, start, end):
         return True
     else:
         return False
+
+
+def find_matching_interval_keys(
+    template_file_path: str, verification_file_path: str
+) -> list:
+    matches = []
+    template_dict = TD_Data_Dictionary(template_file_path)
+    verification_dict = TD_Data_Dictionary(verification_file_path)
+    template_pairs = template_dict.get_key_pairs()
+    verification_pairs = verification_dict.get_key_pairs()
+    template_data = template_dict.calculate_key_interval_time(template_pairs)
+    verification_data = verification_dict.calculate_key_interval_time(
+        verification_pairs
+    )
+    template_keys = template_data.keys()
+    verification_keys = verification_data.keys()
+    for key in template_keys:
+        if key in verification_keys:
+            matches.append(key)
+    return matches
