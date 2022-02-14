49a0bf07ed2680bd78e6115455548a53d4ca7ca4
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2022-02-14 10:35:11 -0500

    Documentation updates and remove unused functions

--- src/utils.py
+++ src/utils.py
@@ -6,11 +6,22 @@
 
 import os
 from log import Logger
-from typing import List
+from typing import List, Any
 from numpy import mean
 
 
-def is_csv_file(path: str):
+def is_csv_file(path: str) -> bool:
+    """
+    Check if a provided path is to a CSV file.
+
+    Parameters
+    ----------
+    path: str
+          The path to be checked.
+    Returns
+    -------
+    bool
+    """
     log = Logger("is_csv_file")
     is_file = os.path.isfile(path)
     if is_file:
@@ -25,43 +36,80 @@ def is_csv_file(path: str):
         return False
 
 
-def chunks(lst, n):
-    """Yield a list of n-sized chunks from lst."""
+def chunks(lst: list, n: int) -> list:
+    """
+    Yield a list of n-sized chunks from a list.
+
+    Parameters
+    ----------
+    text: str
+          The text to be made into a banner.
+    Returns
+    -------
+    None
+    """
     final = [lst[i * n : (i + 1) * n] for i in range((len(lst) + n - 1) // n)]
     return final
 
 
-def pretty_print(dictionary):
+def pretty_print(dictionary: dict) -> None:
+    """
+    Pretty print a dictionary.
+
+    Parameters
+    ----------
+    dictionary: dict
+          The dictionary to be pretty printed.
+    Returns
+    -------
+    None
+    """
     for a, b in dictionary.items():
         print(a, b)
 
 
-def pair_subtract(lst):
+def pair_subtract(lst: list) -> int:
+    """
+    Cleanly subtract the elements of a 2 element list.
+
+    Parameters
+    ----------
+    lst: lst
+          The list to subtract elements from.
+    Returns
+    -------
+    int
+    """
     assert len(lst) == 2
     return lst[1] - lst[0]
 
 
-def n_subtract(*args):
-    sub_res = []
-    for i in args:
-        sub_res.append(pair_subtract(i))
-    return sub_res
+def running_avg(lst: list) -> Any:
+    """
+    Calculate a running average of a list
 
-
-def m_average(*args):
-    """Calculate a running average of a list of lists"""
-    s = 0
-    for element in args:
-        s += element[0] + element[1]
-    return s / 2
-
-
-def running_avg(lst):
-    """Calculate a running average of a list"""
+    Parameters
+    ----------
+    lst: lst
+          The list to find the average of.
+    Returns
+    -------
+    int
+    """
     return mean(lst)
 
 
-def is_float(n: str):
+def is_float(n: str) -> bool:
+    """
+    Checks if a string is a valid float
+    Parameters
+    ----------
+    n: str
+          The string to check.
+    Returns
+    -------
+    bool
+    """
     try:
         float(n)
         return True
@@ -70,6 +118,18 @@ def is_float(n: str):
 
 
 def verify_file_exists(filename: str, extensions: List[str]):
+    """
+    Verify that a file exists at least one of the provided extensions
+    Parameters
+    ----------
+    filename: str
+          The filename to check.
+    extensions: List[str]
+          The possible extensions to use.
+    Returns
+    -------
+    bool
+    """
     filename, file_extension = os.path.splitext(filename)
     if file_extension in extensions:
         return True
@@ -78,6 +138,19 @@ def verify_file_exists(filename: str, extensions: List[str]):
 
 
 def strip_filename(filepath: str, keep_extension: bool = True):
+    """
+    Strip a filename from a path with or without its extension
+    Parameters
+    ----------
+    filepath: str
+          The filepath to use.
+    keep_extension: bool
+          Whether or not to keep the extension of the file.
+          Defaults to True so the extension will be kept
+    Returns
+    -------
+    bool
+    """
     filename = os.path.basename(filepath)
     if keep_extension:
         return filename
