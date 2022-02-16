0dc79ba405fee05f527aeaed2c4b299de815d475
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2022-02-16 00:33:29 -0500

    Add KIT support to the SimilarityVerifier
    This commit adds preliminary KIT support to the SimilarityVerifier.
    This will likely need a little more finetuning but the majority of it is
    there

--- src/verifiers/similarity_verifier.py
+++ src/verifiers/similarity_verifier.py
@@ -6,7 +6,7 @@
 
 import statistics
 from core.log import Logger
-from ..core.td_utils import find_matching_keys
+from ..core.td_utils import find_matching_keys, find_matching_interval_keys
 from core.td_data_dict import TD_Data_Dictionary
 
 # TODO: Add interval_key support
@@ -37,52 +37,107 @@ class SimilarityVerifier:
         sdev = statistics.pstdev(data)
         return sdev
 
-    def find_latency_averages(self, key: str):
+    def find_latency_averages(self, key: str, use_kit=False):
         log = Logger("similarity_find_latency_averages")
         # NOTE: This function does not actually calculate the average latency
         # for the key, rather it perform a lookup on the dictionary returned by calculate_key_hold_time()
         # function for both of the td_data_dict's.
         # This is because the returned KHT dictionary already performs a mean operation if there are
         # multiple latencies for a particular key
-        template_hit_dict = self.template_td_data_dict.calculate_key_hold_time()
-        verification_hit_dict = self.verification_td_data_dict.calculate_key_hold_time()
-        key_matches = find_matching_keys(
-            self.template_file_path, self.verification_file_path
-        )
-        if not key in key_matches:
-            log.km_error("Key %s not found" % key)
-            return
-        t_latency = template_hit_dict.get(key)
-        v_latency = verification_hit_dict.get(key)
-        return [t_latency, v_latency]
+        if use_kit == False:
+            template_hit_dict = self.template_td_data_dict.calculate_key_hold_time()
+            verification_hit_dict = (
+                self.verification_td_data_dict.calculate_key_hold_time()
+            )
+            key_matches = find_matching_keys(
+                self.template_file_path, self.verification_file_path
+            )
+            if not key in key_matches:
+                log.km_error("Key %s not found" % key)
+                return
+            t_latency = template_hit_dict.get(key)
+            v_latency = verification_hit_dict.get(key)
+            return [t_latency, v_latency]
+        elif use_kit == True:
+            template_pairs = self.template_td_data_dict.get_key_pairs()
+            verification_pairs = self.verification_td_data_dict.get_key_pairs()
+            template_hit_dict = self.template_td_data_dict.calculate_key_interval_time(
+                template_pairs
+            )
+            verification_hit_dict = (
+                self.verification_td_data_dict.calculate_key_interval_time(
+                    verification_pairs
+                )
+            )
+            key_matches = find_matching_interval_keys(
+                self.template_file_path, self.verification_file_path
+            )
+            if not key in key_matches:
+                log.km_error("Key %s not found" % key)
+                return
+            # TODO: Double check that this is what we actually want to do here
+            t_latency = template_hit_dict.get(key)
+            v_latency = verification_hit_dict.get(key)
+            return [t_latency, v_latency]
 
-    def calculate_similarity_score(self):
-        matches = find_matching_keys(
-            self.template_file_path, self.verification_file_path
-        )
-        valids = self.find_all_valid_keys()
-        return 1 - (len(valids) / len(matches))
+    def calculate_similarity_score(self, use_kit=False):
+        if use_kit == False:
+            matches = find_matching_keys(
+                self.template_file_path, self.verification_file_path
+            )
+            valids = self.find_all_valid_keys()
+            return 1 - (len(valids) / len(matches))
+        elif use_kit == True:
+            matches = find_matching_interval_keys(
+                self.template_file_path, self.verification_file_path
+            )
+            valids = self.find_all_valid_keys(True)
+            return 1 - (len(valids) / len(matches))
 
-    def is_key_valid(self, key: str) -> bool:
-        latencies = self.find_latency_averages(key)
-        assert len(latencies) == 2
-        sdev = self.calculate_standard_deviation(latencies)
-        # print("The standard deviation is: ", sdev)
-        if sdev <= self.THRESHOLD:
-            return True
-        else:
-            return False
+    def is_key_valid(self, key: str, use_kit=False) -> bool:
+        if use_kit == False:
+            latencies = self.find_latency_averages(key)
+            assert len(latencies) == 2
+            sdev = self.calculate_standard_deviation(latencies)
+            # print("The standard deviation is: ", sdev)
+            if sdev <= self.THRESHOLD:
+                return True
+            else:
+                return False
+        elif use_kit == True:
+            latencies = self.find_latency_averages(key, True)
+            assert len(latencies) == 2
+            sdev = self.calculate_standard_deviation(latencies)
+            # print("The standard deviation is: ", sdev)
+            if sdev <= self.THRESHOLD:
+                return True
+            else:
+                return False
 
-    def find_all_valid_keys(self):
-        valids = []
-        matches = find_matching_keys(
-            self.template_file_path, self.verification_file_path
-        )
-        for key in matches:
-            if self.is_key_valid(key):
-                valids.append(key)
-        print("Valids: ", valids)
-        return valids
+    def find_all_valid_keys(self, use_kit=False):
+        if use_kit == False:
+            valids = []
+            matches = find_matching_keys(
+                self.template_file_path, self.verification_file_path
+            )
+            for key in matches:
+                if self.is_key_valid(key):
+                    valids.append(key)
+            print("Valids: ", valids)
+            return valids
+        elif use_kit == True:
+            valids = []
+            matches = find_matching_interval_keys(
+                self.template_file_path, self.verification_file_path
+            )
+            for key in matches:
+                if self.is_key_valid(key):
+                    valids.append(key)
+            print("Valids: ", valids)
+            return valids
 
-    def count_valid_key_matches(self):
-        return len(self.find_all_valid_keys())
+    def count_valid_key_matches(self, use_kit=False):
+        if use_kit == False:
+            return len(self.find_all_valid_keys())
+        elif use_kit == True:
+            return len(self.find_all_valid_keys(True))
