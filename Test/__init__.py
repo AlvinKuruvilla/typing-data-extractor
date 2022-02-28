e90cd16b51ebb712123ad0299f445e530ea71fa3
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2022-02-28 15:40:39 -0500

    Add more copyright headers

---
+++ Test/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2022, Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
+
+# Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
--- Test/test_word_extractor.py
+++ Test/test_word_extractor.py
@@ -1,3 +1,10 @@
+# Copyright 2022, Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
+
+# Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+
+
 from core.td_data_dict import TD_Data_Dictionary
 from extractors.word_level_extractor import WordExtractor
 import os
